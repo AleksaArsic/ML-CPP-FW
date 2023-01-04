7a3bdd898de58afb09f0ab0a51c03465a07b2799
Aleksa Arsic <arsicaleksa96@gmail.com>
2023-01-04 22:29:16 +0100

    Eigen template library for linear algebra added.

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include "Eigen/Dense"
 #include "test.hpp"
 
 int main()
@@ -7,6 +8,14 @@ int main()
     std::vector<Test> testVector {Test{}, Test{}, Test{}, Test{}, Test{}, Test{}};
 
     std::cout << testVector[0] << std::endl;
+
+    Eigen::MatrixXd m(2, 2);
+    m(0, 0) = 1;
+    m(0, 1) = 2;
+    m(1, 0) = 3;
+    m(1, 1) = 4;
+
+    std::cout << "m = " << std::endl << m << std::endl;
 
     return 0;
 }
