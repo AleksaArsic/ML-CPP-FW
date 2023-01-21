75089dc6025b8149bb56e49daf8702001254eed5
Aleksa Arsic <arsicaleksa96@gmail.com>
2023-01-21 20:08:51 +0100

    build.sh update with check if the build directory already exists.

--- build.sh
+++ build.sh
@@ -18,21 +18,28 @@ USAGE
 # clean build folder and all of it's content
 function clean()
 {
-    echo "Removing old build files"
+    echo "Removing old build files."
     rm -rf build
 }
 
 # use cmake to build project source
 function build()
 {
-    mkdir build
+    if [ -d "./build" ]
+    then 
+        echo "Build folder exists."
+    else
+        mkdir build
+    fi
+    
     cd build 
     cmake ..
     cmake --build .
 }
 
 # if no arguments are provided or more than one argument is provided, return usage function
-if [ $# -eq 0 ] || [ $# -ne 1 ]; then
+if [ $# -eq 0 ] || [ $# -ne 1 ]; 
+then
     usage # run usage function
     exit 1
 fi
@@ -40,12 +47,12 @@ fi
 # start build
 case $1 in
 --clean-build) # start clean build
-    echo "Starting clean build"
+    echo "Starting clean build."
     clean
     build
     ;;
 --build) # standard build
-    echo "Starting build"
+    echo "Starting build."
     build
     ;;
 *)
